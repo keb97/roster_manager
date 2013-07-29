307da701ecd3dae00c5b26b6c109be499063fce5
Kathryn <keb97@cornell.edu>
2013-07-29 09:33:58 -0700

    rails app with capybara, rspec, and factory_gems added to gemfile.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RosterManager::Application.config.secret_token = '1d27b8fb63d71d8634eff1899b1de9111fa7764ec12880dbcc19ebafce45a76ef6576d7a2787d4a7d130ba66b395ac67475da89c3ae69a910a857f168641f97b'
